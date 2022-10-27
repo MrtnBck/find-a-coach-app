56062f3727075612c005daa464a270d9e6877204
Martin Bock <martin.dev.dev@gmail.com>
2022-10-27 16:15:37 +0200

    feat:wiring up pages

---
+++ src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,7 @@
+<template>
+  DETAILS FOR COACH
+  <!-- child routes of coauch detail will be loaded -->
+  <router-view></router-view>
+
+  <router-link to="/coaches/c1/contact">Contact</router-link>
+</template>
