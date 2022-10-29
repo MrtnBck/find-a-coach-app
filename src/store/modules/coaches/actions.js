11a9c779c3c9b036b472a1d5e56bdfe1fe329e2f
Martin Bock <martin.dev.dev@gmail.com>
2022-10-29 09:02:10 +0200

    feat:firebase, PUT to store coach data done

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,8 +1,8 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    //Read the userId from the main store
+    const userId = context.rootGetters.userId;
     const coachData = {
-      //Read the userId from the main store
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
@@ -10,6 +10,20 @@ export default {
       areas: data.areas,
     };
 
-    context.commit("registerCoach", coachData);
+    const response = await fetch(`https://vue-http-demo-2b4db-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`, {
+      method: "PUT",
+      body: JSON.stringify(coachData),
+    });
+
+    //const responseData = await response.json();
+
+    if (!response.ok) {
+      //error
+    }
+
+    context.commit("registerCoach", {
+      ...coachData,
+      id: userId,
+    });
   },
 };
