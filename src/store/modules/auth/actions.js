b806cf6a5af01e158ec6b73bb59311a1070a0cf1
Martin Bock <martin@lobo.at>
2022-11-07 14:16:47 +0100

    feat: Login action and flow done

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,5 +1,32 @@
 export default {
-  login() {},
+  async login(context, payload) {
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA5yzt7PhmscOS_yntM7Z1GIUaUk0U3OqY",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || "Failed to authenticate. Check your login data");
+      console.log(responseData);
+      throw error;
+    }
+
+    console.log(responseData);
+    context.commit("setUser", {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
   async signup(context, payload) {
     const response = await fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA5yzt7PhmscOS_yntM7Z1GIUaUk0U3OqY", {
       method: "POST",
