327ebac557143956d22c71cfd77b9793d0fd18a9
Martin Bock <martin@lobo.at>
2022-11-07 16:03:27 +0100

    feat: adding autologin with LocalStorage

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,34 +1,26 @@
 export default {
   async login(context, payload) {
-    const response = await fetch(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA5yzt7PhmscOS_yntM7Z1GIUaUk0U3OqY",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
-
-    const responseData = await response.json();
-
-    if (!response.ok) {
-      const error = new Error(responseData.message || "Failed to authenticate. Check your login data");
-      console.log(responseData);
-      throw error;
-    }
-
-    console.log(responseData);
-    context.commit("setUser", {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
+    return context.dispatch("auth", {
+      ...payload,
+      mode: "login",
     });
   },
   async signup(context, payload) {
-    const response = await fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA5yzt7PhmscOS_yntM7Z1GIUaUk0U3OqY", {
+    return context.dispatch("auth", {
+      ...payload,
+      mode: "signup",
+    });
+  },
+
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA5yzt7PhmscOS_yntM7Z1GIUaUk0U3OqY";
+
+    if (mode === "signup") {
+      url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA5yzt7PhmscOS_yntM7Z1GIUaUk0U3OqY";
+    }
+
+    const response = await fetch(url, {
       method: "POST",
       body: JSON.stringify({
         email: payload.email,
@@ -41,16 +33,30 @@ export default {
 
     if (!response.ok) {
       const error = new Error(responseData.message || "Failed to authenticate. Check your login data");
-      console.log(responseData);
       throw error;
     }
 
-    console.log(responseData);
+    localStorage.setItem("token", responseData.idToken);
+    localStorage.setItem("userId", responseData.localId);
+
     context.commit("setUser", {
       token: responseData.idToken,
       userId: responseData.localId,
       tokenExpiration: responseData.expiresIn,
     });
+  },
+
+  tryLogin(context) {
+    const token = localStorage.getItem("token");
+    const userId = localStorage.getItem("userId");
+
+    if (token && userId) {
+      context.commit("setUser", {
+        token: token,
+        userId: userId,
+        tokenExpiration: null,
+      });
+    }
   },
 
   logout(context) {
